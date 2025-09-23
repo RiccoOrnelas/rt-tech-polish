817681d60ac0dc44cc4e4d218b4213fb5b4e91be
Henrique Ornelas <103331723+RiccoOrnelas@users.noreply.github.com>
2025-09-22 23:02:15 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,49 +3,18 @@ import "./globals.css";
 import Header from "./components/header/page";
 import Footer from "./components/footer/page";
 import WhatsAppButton from "./components/whasBtn/page"
-import Script from 'next/script'
-
-const geist = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import { GoogleTagManager } from '@next/third-parties/google'
+import Head from "next/head";
 export default function RootLayout({ children }) {
+
   return (
-          <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PSZ8X95N');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </Head>
     <html lang="pt-BR">
-
-      <body className={`container ${geist.variable} ${geistMono.variable}`}>
-        {/* Google Tag Manager (noscript) - APENAS ESTE NO BODY */}
+      <GoogleTagManager gtmId="GTM-PSZ8X95N" />
+      <body className="container">
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PSZ8X95N"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSZ8X95N"
+            height="0" width="0" style="display:none;visibility:hidden">
+          </iframe></noscript>
         <header>
           <Header />
         </header>
@@ -62,7 +31,10 @@ export default function RootLayout({ children }) {
           phoneNumber="5511978915000"
           message="Olá! Gostaria de mais informações sobre seus serviços."
         />
+
       </body>
     </html>
+
   )
 }
+
