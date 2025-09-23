e5fe633bd4d0cd2e7669b130a13ffaad2cf39483
Henrique Ornelas <103331723+RiccoOrnelas@users.noreply.github.com>
2025-09-22 22:10:50 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,17 +19,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body className={`container ${geist.variable} ${geistMono.variable}`}>
+        {/* Google Tag Manager - Script LOGO NO INÍCIO do body */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PSZ8X95N');
+          `}
+        </Script>
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe 
+          <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PSZ8X95N"
-            height="0" 
-            width="0" 
-            style={{display:"none", visibility:"hidden"}}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-        
+
         <header>
           <Header />
         </header>
@@ -46,17 +57,6 @@ export default function RootLayout({ children }) {
           phoneNumber="5511978915000"
           message="Olá! Gostaria de mais informações sobre seus serviços."
         />
-
-        {/* Google Tag Manager - Script no início do body */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PSZ8X95N');
-          `}
-        </Script>
 
         {/* Google Ads (gtag.js) - Método otimizado */}
         <Script
