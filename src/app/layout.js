bc9b55237a224bc6915900798e17fd3046a5e738
Henrique Ornelas <rickornelas.ho@gmail.com>
2025-09-23 00:10:35 -0300

    Updating to GTM 6

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,16 +5,36 @@ import Footer from "./components/footer/page";
 import WhatsAppButton from "./components/whasBtn/page"
 import { GoogleTagManager } from '@next/third-parties/google'
 import Head from "next/head";
+import Script from "next/script";
 export default function RootLayout({ children }) {
 
   return (
     <html lang="pt-BR">
-      <GoogleTagManager gtmId="GTM-PSZ8X95N" />
+
+
+
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', "GTM-PSZ8X95N");
+  `,
+        }}
+      />
       <body className="container">
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSZ8X95N"
-            height="0" width="0" style="display:none;visibility:hidden">
-          </iframe></noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSZ8X95N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <header>
           <Header />
         </header>
