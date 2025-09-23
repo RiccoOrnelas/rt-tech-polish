72f7fd155885a832543be06814141af841067697
Henrique Ornelas <rickornelas.ho@gmail.com>
2025-09-23 00:19:39 -0300

    Updating to GTM 7

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,28 +4,15 @@ import Header from "./components/header/page";
 import Footer from "./components/footer/page";
 import WhatsAppButton from "./components/whasBtn/page"
 import { GoogleTagManager } from '@next/third-parties/google'
-import Head from "next/head";
-import Script from "next/script";
+
 export default function RootLayout({ children }) {
 
   return (
     <html lang="pt-BR">
 
 
+      <GoogleTagManager gtmId="GTM-PSZ8X95N" />
 
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', "GTM-PSZ8X95N");
-  `,
-        }}
-      />
       <body className="container">
         <noscript>
           <iframe
