ddb3c84a21e625c2b80a09b36c9d139df393256b
Henrique Ornelas <rickornelas.ho@gmail.com>
2026-02-09 16:16:23 -0300

    Added new budget button, and adjusted things related to responsivity

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import Footer from "./components/footer/page";
 import WhatsAppButton from "./components/whasBtn/page"
 import { GoogleTagManager } from '@next/third-parties/google'
 
-// ✅ METADATA DEVE FICAR AQUI FORA DA FUNÇÃO
 export const metadata = {
   title: 'RT Polimento Técnico Delivery | Serviço Profissional em Domicílio - São Paulo',
   description: 'Polimento técnico profissional com entrega em domicílio em São Paulo. Restauração de faróis, para-choques e superfícies automotivas. Agende online e receba em casa com qualidade garantida.',
@@ -16,6 +15,19 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-82L99XM5LE"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-82L99XM5LE');
+          `,
+        }} />
+      </head>
+
 
       <body className="container">
         <noscript>
